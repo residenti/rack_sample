5971f39afcb6e88baa24d2c19558be7aba214053
residenti <n.queue.r@gmail.com>
2022-10-14 08:12:40 +0900

    Redirect to hello world

--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
 class HelloWorld
   def call(env)
-    if env['PATH_INFO'] == '/hello'
-      [200, {'Content-Type' => 'text/plain'}, ['Hello World!']]
+    if env['PATH_INFO'] == '/'
+      [301, {'Location' => '/hello'}, []]
+    elsif env['PATH_INFO'] == '/hello'
+      [200, {'Content-Type' => 'text/plain'}, ['Hello World']]
     else
       [404, {'Content-Type' => 'text/plain'}, ['Not Found']]
     end
